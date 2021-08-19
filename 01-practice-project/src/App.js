b2dd857146d4e7271903c724762521ecd01fed87
aramtantushyan <aramtant@gmail.com>
2021-08-19 17:40:44 +0400

    finsihed section 8

---
+++ 01-practice-project/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import {UsersList} from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (user) => {
+    setUsersList((prevUsers) => [...prevUsers, user] );
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} /> 
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
